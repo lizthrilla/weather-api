40fb4aea14fd50874e972f68ccde23ebf9ac9682
Liz Tiller <elizabeth.tiller@gmail.com>
2019-05-01 19:47:40 -0400

    needs styling

--- public/main.js
+++ public/main.js
@@ -1,35 +1,41 @@
 // openweather map: https://openweathermap.org/current
-let apiKey = 'APPID=1bef08da028a63f46a900504187045a6'
-
-// let zipCode = ''
-
-let country = 'us'
 let url = 'http://api.openweathermap.org/data/2.5/weather?'
+let apiKey = 'APPID=1bef08da028a63f46a900504187045a6'
+let zipCode = ''
+let country = 'us'
+let city = ''
 
 const getForecastByZip = () => {
-  let input = document.querySelector('.search-by-zip')
+  let input = document.querySelector('input.search-by-zip-input')
   let zipCode = input.value
   fetch(`${url}zip=${zipCode},${country}&${apiKey}&units=imperial`).then(response => {
-    // console.log(response.json())
     return response.json()
   })
     .then(weatherData => {
-      const windSpeed = `Wind Speed: ${weatherData.wind.speed}`
-      const weatherObject = weatherData.weather.pop()
-      const temp = `Temp: ${weatherData.main.temp}`
-      const sky = `${weatherObject.main} - ${weatherObject.description}`
-      document.querySelector('.temp').textContent = temp
-      document.querySelector('.windSpeed').textContent = windSpeed
-      document.querySelector('.sky').textContent = sky
+      const temp = `Temperature: ${weatherData.main.temp}`
+      document.querySelector('.zip .temp').textContent = temp
+      const windSpeed = weatherData.wind.speed
+      document.querySelector('.zip .windSpeed').textContent = `Wind Speed: ${windSpeed}`
+      const sky = weatherData.weather.pop()
+      document.querySelector('.zip .sky').textContent = `${sky.main} - ${sky.description}`
     })
 }
 
-// const getWeatherByCityName = () => {
-//   const call = `${url}q=Tampa&${apiKey}`
-//   fetch(call)
+const getWeatherByCityName = () => {
+  let input = document.querySelector('input.search-by-city-input')
+  let city = input.value
+  const call = `${url}q=${city}&${apiKey}&units=imperial`
 
-//   console.log(call)
-// }
+  fetch(call).then(response => response.json())
+  .then(json => {
+    const temp = json.main.temp
+    document.querySelector('.city .temp').textContent = `Temperature: ${temp}`
+    const windSpeed = json.wind.speed
+    document.querySelector('.city .windSpeed').textContent = `Wind Speed: ${windSpeed}`
+    const sky = json.weather[0]
+    document.querySelector('.city .sky').textContent = `${json.weather[0].main} - ${json.weather[0].description}`
+  })
+}
 
 // const getRandomBeer = () => {
 //   fetch('https://api.punkapi.com/v2/beers/random').then(response => {
@@ -41,12 +47,12 @@ const getForecastByZip = () => {
 // }
 
 const main = () => {
-  // getRandomBeer()
-  // getWeatherByCityName()
-  let zipSearchButton = document.querySelector('.search-by-zip-button')
+  getWeatherByCityName()
+  let zipSearchButton = document.querySelector('button.search-by-zip-button')
   zipSearchButton.addEventListener('click', getForecastByZip)
-  // if (document.querySelector('h1.hello-world')) {
-  //   document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  
+  let citySearchButton = document.querySelector('button.search-by-city-button')
+  citySearchButton.addEventListener('click', getWeatherByCityName)
   // }
 }
 
